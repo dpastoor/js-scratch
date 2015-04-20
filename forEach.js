8b112e23aea2426ef3b5d4b9b5b0240d3722cea9
Devin Pastoor <devin.pastoor@gmail.com>
2015-04-20 14:07:37 -0400

    add function version as well

--- forEach.js
+++ forEach.js
@@ -3,3 +3,14 @@ var sArray = ["hello", "there", "you"];
 sArray.forEach(function anon(element, index,array) {
   console.log(index + ":" + element);
 });
+
+for (var i=0; i < sArray.length; i++) {
+    console.log(i + ":" + sArray[i]);
+}
+
+var logArray = function(array) {
+  for (var i=0; i < array.length; i++) {
+      console.log("infun" + i + ":" + array[i]);
+  }
+};
+logArray(sArray);
