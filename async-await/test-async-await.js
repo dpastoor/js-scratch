7e71f305d985b1cd50343df798b02563c12eaed5
Devin Pastoor <devin.pastoor@gmail.com>
2016-01-26 12:42:59 -0800

    async await working nicely to count files in the dir as example

--- async-await/test-async-await.js
+++ async-await/test-async-await.js
@@ -1,3 +1,24 @@
 /**
  * Created by devin on 1/26/16.
  */
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs')); // adds Async() versions that return promises
+var path = require('path');
+var _ = require('lodash');
+//
+///** Returns the number of files in the given directory. */
+var countFiles = async function(dir) {
+  var files = await fs.readdirAsync(dir);
+  console.log(files)
+  var paths = _.map(files, function (file) { return path.join(dir, file); });
+  console.log(paths)
+  var stats = _.map(paths, function (path) { return fs.statAsync(path).then(res => res.isFile()) }); // parallel!
+  let results = await* stats;
+  return _.filter(results, (res) => res).length
+  //return _.filter(stats, function (stat) { return stat.isFile(); }).length;
+}
+//
+// Give it a spin
+countFiles(__dirname)
+  .then (function (num) { console.log('There are ' + num + ' files in ' + __dirname); })
+  .catch(function (err) { console.log('Something went wrong: ' + err); });
